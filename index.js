d00453761dfe6fe7b638f28b3204e5f9e141f220
zhong666 <aa900031@gmail.com>
2023-02-15 01:50:24 +0800

    feat: indent tabs

---
+++ index.js
@@ -0,0 +1,9 @@
+module.exports = {
+	extends: '@antfu',
+	rules: {
+		'no-tabs': 0,
+		'indent': ['error', 'tab'],
+		'jsonc/indent': ['error', 'tab'],
+		'@typescript-eslint/indent': ['error', 'tab'],
+	},
+}
