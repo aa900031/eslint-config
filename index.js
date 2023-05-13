9804c044c7b22e3b004268efc74a0a9a5ed9d993
zhong666 <aa900031@gmail.com>
2023-05-14 04:37:34 +0800

    fix: indent

--- index.js
+++ index.js
@@ -1,10 +1,32 @@
+const configTs = require('@antfu/eslint-config-ts')
+const configBasic = require('@antfu/eslint-config-basic')
+
+const hasTs = (() => {
+	try {
+		const ts = require('typescript')
+		return !!ts
+	} catch (e) {
+		return false
+	}
+})()
+
+const indentOpts = () => ({
+	'jsonc/indent': ['error', 'tab'],
+	'vue/html-indent': ['error', 'tab'],
+	...(hasTs ? {
+		'@typescript-eslint/indent': [configTs.rules['@typescript-eslint/indent'][0], 'tab', configTs.rules['@typescript-eslint/indent'][2]],
+	} : {
+		'indent': [configBasic.rules.indent[0], 'tab', configBasic.rules.indent[2]],
+	})
+})
+
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
 	extends: '@antfu',
 	rules: {
+		'eqeqeq': ['error', 'smart'],
 		'no-tabs': 0,
-		'indent': ['error', 'tab'],
-		'jsonc/indent': ['error', 'tab'],
-		'vue/html-indent': ['error', 'tab'],
-		'@typescript-eslint/indent': ['error', 'tab'],
+		'no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+		...indentOpts(),
 	},
 }
