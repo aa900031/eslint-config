4acd713f2498c1a1b376324dc51ff4ecc5d2b172
zhong666 <aa900031@gmail.com>
2023-10-05 00:21:49 +0800

    feat: config of indent

--- src/config.ts
+++ src/config.ts
@@ -1,15 +1,27 @@
-import { antfu, OptionsConfig } from '@antfu/eslint-config'
+import { defu } from 'defu'
+import type { FlatESLintConfigItem, OptionsConfig } from '@antfu/eslint-config'
+import { antfu } from '@antfu/eslint-config'
+import { indent } from './configs/indent'
 
 export type Options =
 	& OptionsConfig
-	& {
-
-	}
 
 export function aa900031(
-	options: Options,
+	options: Options = {},
 ) {
+	const configs: FlatESLintConfigItem[][] = []
+	const overrides: Record<string, FlatESLintConfigItem['rules']>[] = []
+
+	const _indent = indent()
+	configs.push(_indent.configs)
+	overrides.push(_indent.overrides)
+
+	options = defu(options, {
+		overrides: defu(options.overrides, ...overrides),
+	})
+
 	return antfu(
 		options,
+		...configs,
 	)
 }
---
+++ src/configs/indent.ts
@@ -0,0 +1,31 @@
+import type { FlatESLintConfigItem } from '@antfu/eslint-config'
+import { stylistic } from '@antfu/eslint-config'
+
+export function indent(): {
+	configs: FlatESLintConfigItem[]
+	overrides: NonNullable<FlatESLintConfigItem['rules']>
+} {
+	const _stylistic = stylistic()
+	const ruleIndent = _stylistic[0].rules?.['style/indent']
+
+	return {
+		configs: [
+			{
+				name: 'aa900031:indent:stylistic',
+				rules: {
+					'style/no-tabs': 0,
+					'style/no-mixed-spaces-and-tabs': ['error', 'smart-tabs'],
+					'style/indent': [ruleIndent[0], 'tab', ruleIndent[2]],
+				},
+			},
+		],
+		overrides: {
+			vue: {
+				'vue/html-indent': ['error', 'tab'],
+			},
+			jsonc: {
+				'jsonc/indent': ['error', 'tab'],
+			},
+		},
+	}
+}
