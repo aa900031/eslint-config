d63eabb41406555a13e09df442e7ab8bca2f6231
zhong666 <aa900031@gmail.com>
2023-10-04 22:33:57 +0800

    feat!: move to eslint flat config

---
+++ src/config.ts
@@ -0,0 +1,15 @@
+import { antfu, OptionsConfig } from '@antfu/eslint-config'
+
+export type Options =
+	& OptionsConfig
+	& {
+
+	}
+
+export function aa900031(
+	options: Options,
+) {
+	return antfu(
+		options,
+	)
+}
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { aa900031 } from './config'
+
+export * from './config'
+export default aa900031
