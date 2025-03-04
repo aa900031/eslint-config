2e48ce5dc3aa3fb76d88ff537be6d75d5d6434f2
zhong666 <aa900031@gmail.com>
2025-03-04 22:18:40 +0800

    feat!: drop CJS build

--- build.config.ts
+++ build.config.ts
@@ -5,11 +5,14 @@ export default defineBuildConfig({
 		'src/index',
 	],
 	clean: true,
-	declaration: true,
-	rollup: {
-		emitCJS: true,
-	},
+	declaration: 'node16',
 	externals: [
 		'eslint-flat-config-utils',
 	],
+	hooks: {
+		'rollup:dts:options': (ctx, options) => {
+			// @ts-expect-error - remove commonjs plugin
+			options.plugins = options.plugins.filter(i => i?.name !== 'commonjs')
+		},
+	},
 })
